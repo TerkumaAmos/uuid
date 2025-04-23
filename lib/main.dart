85c5b6b31e7a2243d1fba33cb6f3d0d45985977f
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-23 02:22:16 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:hate/counter.dart';
 import 'package:provider/provider.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ChangeNotifierProvider(
-//       create: (context) => Todo(),
-//       child: const MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: TodoPage(),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Todo(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: TodoPage(),
+      ),
+    );
+  }
+}
 
 // import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
